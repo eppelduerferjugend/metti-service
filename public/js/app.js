08e302dcae607f3383e46ac62062383cdb026a01
Thierry Degeling <thierry.degeling@govcert.etat.lu>
2019-08-22 22:01:12 +0200

    js fix

--- public/js/app.js
+++ public/js/app.js
@@ -30578,8 +30578,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/thierry/repos/metti/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/thierry/repos/metti/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/vagrant/code/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/vagrant/code/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
